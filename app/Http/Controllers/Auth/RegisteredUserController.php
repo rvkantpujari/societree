1d1a89be0ca821e4ea202260402fdb858734b3a8
Ravi Kant Pujari <rvkantpujari@gmail.com>
2024-05-08 02:05:07 -0400

    add: validation on registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,13 +30,24 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'first_name' => ['required', 'string', 'min:2', 'max:50'],
+            'last_name' => ['required', 'string', 'min:2', 'max:50'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ], [
+            'first_name.required' => 'First Name is required.',
+            'first_name.min' => 'First Name must be at least :min characters long.',
+            'first_name.max' => 'First Name must not be greater than :max characters.',
+            'last_name.required' => 'Last Name is required.',
+            'last_name.min' => 'Last Name must be at least :min characters long.',
+            'last_name.max' => 'Last Name must not be greater than :max characters.',
+            'email.required' => 'Email is required.',
+            'email.email' => 'Email must be a valid email address.'
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
