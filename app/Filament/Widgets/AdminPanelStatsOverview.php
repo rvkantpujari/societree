506566ccf217a66fc508b183c5207fe5398fb7a0
Ravi Kant Pujari <rvkantpujari@gmail.com>
2025-02-10 08:08:00 -0400

    add: users count admin panel stats widget

--- app/Filament/Widgets/AdminPanelStatsOverview.php
+++ app/Filament/Widgets/AdminPanelStatsOverview.php
@@ -2,15 +2,23 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\User;
+use Filament\Support\Enums\IconPosition;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class AdminPanelStatsOverview extends BaseWidget
 {
+    protected ?string $heading = 'Analytics';
+
+    protected ?string $description = 'An overview of statistics.';
+
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make('Users', count(User::all()) - 1)
+                ->description('Total Registered Users')
+                ->descriptionIcon('heroicon-m-users', IconPosition::Before),
         ];
     }
 }
