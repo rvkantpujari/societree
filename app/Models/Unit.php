6d74d994ecf592482439d8d02d5f5a9250fa7c3e
Ravi Kant Pujari <rvkantpujari@gmail.com>
2025-02-17 19:53:02 -0400

    fix: unit model timezone issue

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
 
 class Unit extends Model
 {
@@ -18,4 +20,28 @@ class Unit extends Model
         'unit_name',
         'block_id',
     ];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return $this->convertToUserTimezone($value);
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return $this->convertToUserTimezone($value);
+    }
+
+    // Do NOT override getDeletedAtAttribute()
+    public function getDeletedAttribute($value)
+    {
+        return $this->deleted_at
+            ? $this->convertToUserTimezone($this->deleted_at)
+            : null;
+    }
+
+    private function convertToUserTimezone($value)
+    {
+        $timezone = Auth::check() ? Auth::user()->timezone : config('app.timezone');
+        return Carbon::parse($value)->tz($timezone)->toDateTimeString();
+    }
 }
