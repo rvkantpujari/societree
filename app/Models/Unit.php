3b8170861540101111bdb4dfe514695c7bd07f4d
Ravi Kant Pujari <rvkantpujari@gmail.com>
2025-02-17 19:39:57 -0400

    add: unit model and migration

---
+++ app/Models/Unit.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Unit extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'unit_name',
+        'block_id',
+    ];
+}
